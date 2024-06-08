c2bd2b98efb9b63bee3255c86b350fbb6a936647
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2024-06-08 14:56:34 +0900

    add playRound function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,52 @@
+"use strict";
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * (4 - 1) + 1);
+    const result = Math.floor(Math.random() * 3) + 1;
+
+    if( result === 1 ) {
+        return console.log("rock");
+    } else if( result === 2 ) {
+        return console.log("paper");
+    } else {
+        return console.log("scissor");
+    }
+    
 }
 
-let result = getComputerChoice();
 
-if(result === 1) {
-    return console.log("Rock");
-} else if( result === 2) {
-    return console.log("Paper");
-} else {
-    return console.log("Scissor");
+function getHumanChoice() {
+    let choice;
+    const validChoices = ["rock", "paper", "scissor"];
+    
+    do {
+        choice = prompt("Rock, paper, or scissor?").toLowerCase(); 
+    } while( !validChoices.includes(choice) );
+
+    return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice === computerChoice) {
+    return console.log("draw");
+  } else if("scissor", "paper" || "paper", "rock") {
+    console.log("You won!");
+    return humanScore++
+  } else {
+    console.log("You lose!");
+    return computerScore++
+  } 
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+function playGame() {
+
+}
+
+console.log(`Player score: ${humanScore} \n Computer score: ${computerScore}`);
