48df3b1a613d5c348fd61d38fda9dbeece30e927
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2024-06-08 16:18:47 +0900

    edit javascript.js

--- javascript.js
+++ javascript.js
@@ -6,10 +6,10 @@ let computerScore = 0;
 function getComputerChoice() {
     const result = Math.floor(Math.random() * 3) + 1;
 
-    if( result === 1 ) {
+    if (result === 1) {
         console.log("Computer chose: rock");
         return "rock";
-    } else if( result === 2 ) {
+    } else if (result === 2) {
         console.log("Computer chose: paper")
         return "paper";
     } else {
@@ -25,21 +25,21 @@ function getHumanChoice() {
     const validChoices = ["rock", "paper", "scissor"];
 
     do {
-      choice = prompt(`Rock, paper, or scissor? (5 rounds)`); 
+      choice = prompt(`Rock, paper, or scissor? (5 rounds game)`); 
       if (choice === null) {
         console.log("You quit the game");
         return null;
       }
       choice = choice.toLowerCase();
-    } while( !validChoices.includes(choice) );
+    } while (!validChoices.includes(choice));
 
     return choice;
 }
 
   function playRound(humanChoice, computerChoice) {
-    if(humanChoice === computerChoice) {
+    if (humanChoice === computerChoice) {
     return console.log("draw");
-  } else if(
+  } else if (
     (humanChoice === "scissor" && computerChoice === "paper") || 
     (humanChoice === "paper" && computerChoice === "rock") || 
     (humanChoice === "rock" && computerChoice === "scissor") 
@@ -56,28 +56,29 @@ function playGame() {
   let i = 0;
   do {
     const humanSelection = getHumanChoice();
-    if(humanSelection === null) {
+
+    if (humanSelection === null) {
       console.log("Game ended");
       return;
     }
+
     const computerSelection = getComputerChoice();  
+
     playRound(humanSelection, computerSelection);
     console.log(`Player score: ${humanScore} \nComputer score: ${computerScore}`);
     i++;
   } while (i < 5);
-  finalScore();
+    finalScore();
 }
 
 function finalScore() {
-  if(humanScore > 2) {
+  if (humanScore > computerScore) {
     console.log("Congrats! You won!");
-  } else if (humanScore <= 2) {
+  } else if (humanScore < computerScore) {
     console.log("You lost. Try again.");
-  } else if (humanScore === computerScore) {
-    console.log("Draw. Try again.");
   } else {
-    console.log("Try again?");
+    console.log("Draw. Try again.");
   }
 }
 
-console.log(playGame());
+playGame();
